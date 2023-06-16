8899b4b76cd2a5af4b16f6da7bc1abc9ae2f8807
jpatel345 <jpatel345@myseneca.ca>
2023-06-16 16:27:16 -0400

    commit

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-/*********************************************************************************
-
-WEB322 – Assignment 02
-I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
-
-Name: JIGAR PATEL
-Student ID: 156200214
-Date: 2 JUNE 2023
-Cyclic Web App URL: https://courageous-pig-coveralls.cyclic.app/
-GitHub Repository URL: https://github.com/jiga-rr/WEB322-app
-
-********************************************************************************/ 
-
-
 const express = require('express');
 const app = express();
 const storeService = require('./store-service');
+const path = require('path');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: 'Your_Cloud_Name',
+  api_key: 'Your_API_Key',
+  api_secret: 'Your_API_Secret',
+  secure: true
+});
+
+// Multer upload configuration
+const upload = multer();
 
 app.use(express.static('public'));
 
@@ -23,7 +24,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/views/about.html');
+  res.sendFile(path.join(__dirname, '/views/about.html'));
 });
 
 app.get('/shop', (req, res) => {
@@ -33,9 +34,19 @@ app.get('/shop', (req, res) => {
 });
 
 app.get('/items', (req, res) => {
-  storeService.getAllItems()
-    .then(items => res.json(items))
-    .catch(err => res.json({ message: err }));
+  if (req.query.category) {
+    storeService.getItemsByCategory(parseInt(req.query.category))
+      .then(items => res.json(items))
+      .catch(err => res.json({ message: err }));
+  } else if (req.query.minDate) {
+    storeService.getItemsByMinDate(req.query.minDate)
+      .then(items => res.json(items))
+      .catch(err => res.json({ message: err }));
+  } else {
+    storeService.getAllItems()
+      .then(items => res.json(items))
+      .catch(err => res.json({ message: err }));
+  }
 });
 
 app.get('/categories', (req, res) => {
@@ -44,17 +55,73 @@ app.get('/categories', (req, res) => {
     .catch(err => res.json({ message: err }));
 });
 
+app.get('/items/add', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/addItem.html'));
+});
+
+app.post('/items/add', upload.single('featureImage'), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    upload(req)
+      .then((uploaded) => {
+        processItem(uploaded.url);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ message: 'Error uploading image' });
+      });
+  } else {
+    processItem('');
+  }
+
+  function processItem(imageUrl) {
+    req.body.featureImage = imageUrl;
+
+    storeService.addItem(req.body)
+      .then((item) => {
+        res.redirect('/items');
+      })
+      .catch((err) => {
+        res.json({ message: err });
+      });
+  }
+});
+
+app.get('/item/:id', (req, res) => {
+  storeService.getItemById(parseInt(req.params.id))
+    .then(item => res.json(item))
+    .catch(err => res.json({ message: err }));
+});
+
 app.get('*', (req, res) => {
-  res.status(404).send('Page Not Found');
+  res.sendFile(path.join(__dirname, '/views/404.html'));
 });
 
 storeService.initialize()
   .then(() => {
-    const port = process.env.PORT || 8080;
-    app.listen(port, () => {
-      console.log(`Express http server listening on port ${port}`);
+    app.listen(3000, () => {
+      console.log('Server is running on http://localhost:3000');
     });
   })
-  .catch(err => {
-    console.error(err);
+  .catch((err) => {
+    console.error('Initialization failed:', err);
   });
